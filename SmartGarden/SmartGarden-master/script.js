08f7d63a12dc35b6ed892276c3ccc362c488f5ef
Daniel Święcki <69246123+Daniel-kormoran@users.noreply.github.com>
2024-06-16 16:37:48 +0200

    Delete SmartGarden/SmartGarden-master directory

--- SmartGarden/SmartGarden-master/script.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button) => {
-	button.addEventListener("click", () => {
-		console.log(`Kliknięto przycisk: ${button.textContent}`);
-	});
-});
